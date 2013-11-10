747ac3f97f940ac2f10718a16831cfe2fb171ad7
Billy Lee <doohyunl@middlebury.edu>
2013-11-10 07:55:52 -1000

    added Address model

--- cloudtestenvironment/src/cloudtestenvironment/models.py
+++ cloudtestenvironment/src/cloudtestenvironment/models.py
@@ -3,19 +3,6 @@ from cloudtestenvironment import app
 
 db = SQLAlchemy(app)
 
-class Contact(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(40))
-	email = db.Column(db.String(40))
-	phone = db.Column(db.String(15))
-	message = db.Column(db.String(500))
-
-	def __init__(self, name, email, phone, message):
-		self.name = name
-		self.email = email
-		self.phone = phone
-		self.message = message
-
 class Customer(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(40))
@@ -23,11 +10,34 @@ class Customer(db.Model):
 	phone = db.Column(db.String(15))
 	company = db.Column(db.String(40))
 	registered = db.Column(db.Integer)
+	addresses = relationship("Address")
 
-	def __init__(self, name, email, phone, company, registered=1):
+	def __init__(self, name, email, phone, company, registered=0):
 		self.name = name
 		self.email = email
 		self.phone = phone
 		self.company = company
 		self.registered = registered
+
+class Address(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	address1 = db.Column()
+	address2 = db.Column()
+	city = db.Column()
+	state = db.Column()
+	country = db.Column()
+	zip_code = db.Column()
+	address_type = db.Column()
+	customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
+
+	def __init__(self, address1, address2, city, state, country, 
+				zip_code, address_type, customer_id):
+		self.address1 = address1
+		self.address2 = address2
+		self.city = city
+		self.state = state
+		self.country = country
+		self.zip_code = zip_code
+		self.address_type = address_type
+		self.customer_id = customer_id
 
