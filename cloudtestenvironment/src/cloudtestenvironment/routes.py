6c0b4a21f57338e9cd56ba80fb6e9094c9840e0a
acthomas06 <REDXVII@Andrews-MacBook-Pro-3.local>
2013-11-12 12:12:28 -0700

    cleaned up routes.py and added contact logic

--- cloudtestenvironment/src/cloudtestenvironment/routes.py
+++ cloudtestenvironment/src/cloudtestenvironment/routes.py
@@ -30,41 +30,9 @@ def landing_submit():
 	if registration_form.sign_up.data == True:
 		if registration_form.validate_on_submit():
 			return redirect('register')
-	if registration_form.validate_on_submit():
-		#TODO: save registration
-		customer = Customer(
-			name = registration_form.name.data,
-			email = registration_form.email.data,
-			phone = registration_form.phone.data,
-			company = registration_form.company.data
-		)
-		if registration_form.tell_me_more.data == True:
-			customer.registered = 0
-			db.session.add(customer)
-			db.session.commit()
-			return redirect(url_for('details', _anchor='registered')) #TODO: we can't remove the anchor
-		if registration_form.sign_up.data == True:
-			db.session.add(customer)
-			db.session.commit()
-			return redirect(url_for('order', _anchor='registered'))
-	else:
-		session['name'] = registration_form.name.data
-		session['email'] = registration_form.email.data
-		session['phone'] = registration_form.phone.data
-		session['company'] = registration_form.company.data
 
 	contact_form = ContactForm()
-	if contact_form.validate_on_submit():
-		# TODO: save message
-		contact = Contact(
-			name = contact_form.name.data,
-			email = contact_form.email.data,
-			phone = contact_form.phone.data,
-			message = contact_form.message.data
-		)
-		db.session.add(contact)
-		db.session.commit()
-		return "message sent"
+
 	if contact_form.send.data == True:
 		if contact_form.validate_on_submit():
 			return redirect('contact')
@@ -190,8 +158,25 @@ def register():
 @app.route('/contact')
 @app.route('/contact.html')
 def contact_message():
-	content = render_template('contact_confirmation.html')
-	return content
+	contact_form = ContactForm()
+
+	customer = Customer(
+			name = contact_form.name.data,
+			email = contact_form.email.data,
+			phone = contact_form.phone.data,
+			company = contact_form.company.data
+		)
+	if not contact_form.validate_on_submit():
+		customer.registered == False
+		db.session.add(customer)
+		db.session.commit()
+		return redirect(url_for('landing'))
+	else:
+		db.session.add(customer)
+		db.session.commit()
+		return "message sent"
+		content = render_template('contact_confirmation.html')
+		return content
 
 with app.test_request_context():
 	print "starting application"
