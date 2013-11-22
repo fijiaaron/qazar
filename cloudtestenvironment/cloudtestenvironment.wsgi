582eeda1f1aa20092bf8480d5b68139c41544735
Aaron Evans <fijiaaron@fast-mail.org>
2013-11-22 15:58:51 -0700

    modified wsgi script to use relative path

--- cloudtestenvironment/cloudtestenvironment.wsgi
+++ cloudtestenvironment/cloudtestenvironment.wsgi
@@ -1,4 +1,6 @@
-import sys
-sys.path.insert(0, '/home/aaron/cloudtestenvironment')
+import os, sys
+
+cloudtestenvironment = os.path.abspath(".")
+sys.path.insert(0, cloudtestenvironment)
 
 from cloudtestenvironment import app as application
--- cloudtestenvironment/test/controllers/test_registration_controller.py
+++ cloudtestenvironment/test/controllers/test_registration_controller.py
@@ -3,18 +3,17 @@ from cloudtestenvironment.controllers import RegistrationController
 
 from pprint import pprint as pp
 
-#ready to start testing and implementing
-
+registration = None
 def setup():
 	print("")
 	print("creating registration controller")
+	global registration
 	registration = RegistrationController(app.config)
 	registration.set_db(db)
 	pp(registration)
 
-registration = RegistrationController(None)
-registration.set_db(db)
 
-fixture = { 'registration' : registration }
 def test_registration_has_db():
 	assert registration.db
+
+	
